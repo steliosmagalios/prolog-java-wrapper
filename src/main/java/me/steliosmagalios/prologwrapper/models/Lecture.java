b7d3aefb5e13a67228056856ba3960b349662f6a
Stelios Magalios <47584641+steliosmagalios@users.noreply.github.com>
2022-09-03 18:40:23 +0300

    missing getters/setters

--- src/main/java/me/steliosmagalios/prologwrapper/models/Lecture.java
+++ src/main/java/me/steliosmagalios/prologwrapper/models/Lecture.java
@@ -1,5 +1,7 @@
 package me.steliosmagalios.prologwrapper.models;
 
+import java.util.Arrays;
+
 public class Lecture {
 
     private int id;
@@ -8,4 +10,56 @@ public class Lecture {
     private int[] professors;
     private int[] groups;
 
+    public Lecture() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public int[] getProfessors() {
+        return professors;
+    }
+
+    public void setProfessors(int[] professors) {
+        this.professors = professors;
+    }
+
+    public int[] getGroups() {
+        return groups;
+    }
+
+    public void setGroups(int[] groups) {
+        this.groups = groups;
+    }
+
+    @Override
+    public String toString() {
+        return "Lecture{" +
+                "id=" + id +
+                ", duration=" + duration +
+                ", roomType='" + roomType + '\'' +
+                ", professors=" + Arrays.toString(professors) +
+                ", groups=" + Arrays.toString(groups) +
+                '}';
+    }
 }
